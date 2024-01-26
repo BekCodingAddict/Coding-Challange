813ab1f3c7c88f3dbf6d83dee3cd34217469ebdf
BekCodingAddict <experte001@gmail.com>
2024-01-26 22:19:30 +0900

    Remainder from Two Number and Find the Peremeter of rectangle

---
+++ JavaScript/Very Easy/14.Find_the_Peremeter_of_Rectangle.js
@@ -0,0 +1,20 @@
+// Find the Perimeter of a Rectangle
+/*
+Create a function that takes length and width and finds the perimeter of a rectangle.
+Examples:
+findPerimeter(6, 7) ➞ 26
+findPerimeter(20, 10) ➞ 60
+findPerimeter(2, 9) ➞ 22
+Notes:
+Don't forget to return the result.
+*/
+
+function findPerimeter(length, width){
+    
+}
+
+
+//Don't touch this code lines!!!
+findPerimeter(6,7)===26? console.log('Test Passed! Result:'+findPerimeter(6,7)):console.log('Test Failed! Result:'+findPerimeter(6,7));
+findPerimeter(20,10)===60? console.log('Test Passed! Result:'+findPerimeter(20,10)):console.log('Test Failed! Result:'+findPerimeter(20,10));
+findPerimeter(2,9)===26? console.log('Test Passed! Result:'+findPerimeter(2,9)):console.log('Test Failed! Result:'+findPerimeter(2,9));

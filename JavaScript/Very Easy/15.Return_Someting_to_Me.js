18cd9be860d404e55501494aa8edb077022374ff
BekCodingAddict <experte001@gmail.com>
2024-01-30 00:39:25 +0900

    Creating return something function

---
+++ JavaScript/Very Easy/15.Return_Someting_to_Me.js
@@ -0,0 +1,21 @@
+// Return something to me
+/* 
+Write a function that returns the string "something" joined with a space " " and the given argument a.
+Examples
+giveMeSomething("is better than nothing") ➞ "something is better than nothing"
+giveMeSomething("Bob Jane") ➞ "something Bob Jane"
+giveMeSomething("something") ➞ "something something"
+
+Notes:
+Assume an input is given.
+*/
+
+function giveMeSomething(a) {
+	
+}
+
+
+//Don't touch this code!
+giveMeSomething('is better than nothing').includes('something')===true? console.log('Test Passed! Result:'+giveMeSomething('is better than nothing').includes('something')):console.log('Test Failed! Result:'+giveMeSomething('is better than nothing').includes('something'));
+giveMeSomething('Bob Jane').includes('something')===true? console.log('Test Passed! Result:'+giveMeSomething('Bob Jane').includes('something')):console.log('Test Failed! Result:'+giveMeSomething('Bob Jane').includes('something'));
+giveMeSomething('something').includes('something')===true? console.log('Test Passed! Result!'+giveMeSomething('something').includes('something')):console.log('Test Failed! Result: '+giveMeSomething('something').includes('something'));
